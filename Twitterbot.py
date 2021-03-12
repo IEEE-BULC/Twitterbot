317e31aa692c329cf17fcbea266460b20df094bf
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-12 20:38:43 +0500

    Update Twitterbot.py

--- Twitterbot.py
+++ Twitterbot.py
@@ -10,3 +10,6 @@ auth.set_access_token(key, secret)
 
 api = tweepy.API(auth)
 api.update_status('Twitter bot reporing in live')
+api = tweepy.API(auth)
+tweets= API.mentions_timeline()
+print(tweets[0].text)
