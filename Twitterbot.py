f8b43a02e1f7fb5366a8d4110f709383698d2273
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-12 20:38:19 +0500

    Update Twitterbot.py

--- Twitterbot.py
+++ Twitterbot.py
@@ -1,1 +1,9 @@
 import tweepy
+consumer_key ='consumer key here'
+consumer_secret = 'consumer secret key here'
+
+key = 'consumer key'
+secret= 'secret key '
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
